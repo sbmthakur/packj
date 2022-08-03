59bf5dc785b89af9d816e5daec2882b30b0aa1ce
Ashish Bijlani <ashish@ossillate.com>
2022-08-03 09:55:58 -0500

    Use script to strace --- for testing
    Signed-off-by: Ashish Bijlani <ashish@ossillate.com>

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-cat aaa | while read line
-	do
-		strace -f -e trace=network,file,process -ttt -T -o strace_$line.log pip install --quiet $line
-	done
+strace -f -e trace=network,file,process -ttt -T -o strace.log pip install --quiet tensorflow
 #python3 main.py pypi tensorflow --dynamic
