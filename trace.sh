4b8c12b842451383d6accc418da2621f0533f76c
Shubham Thakur <sbmthakur@gmail.com>
2022-08-04 16:11:24 -0500

    Create venv in trace.sh

--- trace.sh
+++ trace.sh
@@ -1,5 +1,6 @@
 #strace -f -e trace=network,file,process -ttt -T -o strace.log pip install --quiet $2
 #. venv/bin/activate && python3 main.py --dynamic $1 $2
+python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
 python3 main.py --dynamic $1 $2
