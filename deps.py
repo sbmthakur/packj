01496831cea80fecd9d6fb3f125ed1afe7bef4ee
shubham thakur <sbmthakur@gmail.com>
2022-08-18 12:56:19 -0500

    Support NPM in deps

--- deps.py
+++ deps.py
@@ -1,9 +1,9 @@
 import re
 import subprocess
+import json
 
 def __parse_string_for_pkg_info(line):
     try:
-        #breakpoint()
         if line == '':
             return None
         version_search_outcome = re.search(r'(.*)(==|>=|<=)(.*)', line)
@@ -20,25 +20,45 @@ def __parse_string_for_pkg_info(line):
     except Exception as e:
         raise Exception("Failed to parse %s: %s" % (line, str(e)))
 
-def get_packages_from_file(filepath):
+def get_python_packages(filepath):
     packages=[]
+    with open(filepath) as f:
+        while True:
+            line = f.readline().strip()
+            pkg = __parse_string_for_pkg_info(line)
+            if not pkg:
+                break
+            packages.append(pkg)
+    return packages
+
+def get_node_packages(filepath):
+    packages=[]
+    with open(filepath) as f:
+        pkg_data = json.load(f)
+
+        for k,v in pkg_data['dependencies'].items():
+            pkg = {
+                "name": k,
+                "version": v.replace('^', '').replace('~', '')
+            }
+            packages.append(pkg)
+    return packages
+
+def get_packages_from_file(filepath, pm_name):
+    packages = []
     try:
-        with open(filepath) as f:
-            while True:
-                line = f.readline().strip()
-                pkg = __parse_string_for_pkg_info(line)
-                if not pkg:
-                    break
-                packages.append(pkg)
-        #breakpoint()
-        return packages
+        if pm_name == 'pypi':
+            packages = get_python_packages(filepath)
+        elif pm_name == 'npm':
+            packages = get_node_packages(filepath)
+        
     except Exception as e:
         print("Failed to parse %s for packages" % (tmpfile, str(e)))
         pass
     finally:
         return packages
 
-def create_html(total_risks, file_name):
+def create_html(total_risks):
 
 	html = f"<details><summary>{total_risks} issues found with dependencies. Click here for details</summary>"
 	html += "<table><tr><th>Package name</th><th>Description</th><th>Dependency file</th></tr>"
@@ -46,7 +66,7 @@ def create_html(total_risks, file_name):
 		for line in f:
 			if '==' in line:
 				break
-			name, summary = line.split('|')
+			name, summary,file_name = line.split('|')
 			html += f'<tr><td>{name}</td><td>{summary}</td><td>{file_name}</td></tr>'
 		html += '</table></details>'
 
@@ -59,59 +79,62 @@ def create_html(total_risks, file_name):
 	print(f'View complete report: {html_report_name}')
 
 def main(args):
-    file_name = args.file_name
-    packages = get_packages_from_file(file_name)
-
-    pm_name = None
-    if file_name == 'requirements.txt':
-        pm_name = 'pypi'
-    elif file_name == 'package.json':
-        pm_name = 'npm'
-    elif file_name == 'Gemfile.lock':
-        pm_name = 'rubygems'
-    else:
-        pm_name = 'pypi'
-
+    file_names = args.file_name.split(',')
 
     total_risks = 0
 
     with open('./tempfile', mode='w') as f:
-        for pkg_info in packages:
-            name, version = pkg_info['name'], pkg_info['version']
+        for file_name in file_names:
 
-            inputs = ['python', 'main.py', 'audit', pm_name, name]
+            pm_name = None
+            if file_name == 'requirements.txt':
+                pm_name = 'pypi'
+            elif file_name == 'package.json':
+                pm_name = 'npm'
+            elif file_name == 'Gemfile.lock':
+                pm_name = 'rubygems'
+            else:
+                pm_name = 'pypi'
 
-            if version:
-                inputs.append(version)
+            packages = get_packages_from_file(file_name, pm_name)
+            #breakpoint()
 
-            try:
-                o = subprocess.check_output(inputs, stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as e:
-            #except Exception as e:
-                print(f'subprocess failed: {e.output}')
-                continue
+            for pkg_info in packages:
+                name, version = pkg_info['name'], pkg_info['version']
 
-            pkg_result = o.decode('utf-8').split('\n')[-4]
-            pkg_result = pkg_result.replace('[+] ', '')
+                inputs = ['python', 'main.py', 'audit', pm_name, name]
 
-            n = None
+                if version:
+                    inputs.append(version)
 
-            try:
-                n = int(pkg_result[0])
-                total_risks += n
-            except:
-                print(f'Not a numeric character {pkg_result[0]}')
+                try:
+                    o = subprocess.check_output(inputs, stderr=subprocess.STDOUT)
+                except subprocess.CalledProcessError as e:
+                #except Exception as e:
+                    print(f'subprocess failed: {e.output}')
+                    continue
 
+                index = None
 
-            f.write(f'{name}| {pkg_result}\n')
+                if pm_name == 'pypi':
+                    index = -4
+                elif pm_name == 'npm':
+                    index = -3
 
-            #print(name, pkg_result)
+                pkg_result = o.decode('utf-8').split('\n')[index]
+                pkg_result = pkg_result.replace('[+] ', '')
 
+                n = None
+
+                try:
+                    n = int(pkg_result[0])
+                    total_risks += n
+                except:
+                    print(f'Not a numeric character {pkg_result[0]}')
+
+                print(f'{name}| {pkg_result}| {file_name}')
+                f.write(f'{name}| {pkg_result}| {file_name}\n')
+        
         f.write('======')
-    
-    create_html(total_risks, file_name)
 
-
-
-
-    
+    create_html(total_risks)
